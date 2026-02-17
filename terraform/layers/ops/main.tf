36e73bf4bf897cf3c8a8dbbc596cb0887f39fda6
monukoni <kartoplya213@gmail.com>
2026-02-17 23:38:06 +0200

    fixed db naming

--- terraform/layers/ops/main.tf
+++ terraform/layers/ops/main.tf
@@ -6,13 +6,13 @@ module "iam" {
 
 module "studentus_db" {
   source  = "../../modules/rds"
-  name    = "studentus-db"
+  name    = "studentus_db"
   subnets = data.terraform_remote_state.bootstrap.outputs.eks_private_subnets[*].id
 }
 
 module "backend_data_protection_db" {
   source  = "../../modules/rds"
-  name    = "backend-data-protection-db"
+  name    = "backend_data_protection_db"
   subnets = data.terraform_remote_state.bootstrap.outputs.eks_private_subnets[*].id
 }
 
