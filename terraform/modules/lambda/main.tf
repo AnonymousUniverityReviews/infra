1f0c6cb5b173fa5fbd0fec7248506b58b370cec4
monukoni <kartoplya213@gmail.com>
2026-02-18 19:05:22 +0200

    make lambda as docker image

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -20,11 +20,11 @@ resource "aws_lambda_function" "example" {
   function_name = var.name
   role          = aws_iam_role.role.arn
   package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.example.repository_url}:latest"
+  image_uri     = "585517712585.dkr.ecr.eu-central-1.amazonaws.com/dbmigrations:latest"
 
-  image_config {
-    command = ["main.handler"]
-  }
+#   image_config {
+#     command = ["main.handler"]
+#   }
 
   memory_size = 512
   timeout     = 30
