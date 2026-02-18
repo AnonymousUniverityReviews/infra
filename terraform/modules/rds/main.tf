a7765d173ec9bed94216aa3c6b6dc9da7c11c867
monukoni <kartoplya213@gmail.com>
2026-02-18 16:03:13 +0200

    updated db naming

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -4,17 +4,19 @@ resource "aws_db_subnet_group" "db" {
 }
 
 resource "aws_db_instance" "db" {
-  allocated_storage           = 10
-  db_name                     = var.name
-  engine                      = "postgres"
-  engine_version              = "17.6"
-  instance_class              = "db.t4g.micro"
-  manage_master_user_password = true
-  username                    = "postgres"
-  parameter_group_name        = "default.postgres17"
-  multi_az                    = false
-  storage_encrypted           = true
-  publicly_accessible         = false
-  db_subnet_group_name        = aws_db_subnet_group.db.name
-  skip_final_snapshot         = true
+  allocated_storage                   = 10
+  identifier                          = var.name
+  db_name                             = var.name
+  engine                              = "postgres"
+  engine_version                      = "17.6"
+  instance_class                      = "db.t4g.micro"
+  manage_master_user_password         = true
+  username                            = "postgres"
+  parameter_group_name                = "default.postgres17"
+  multi_az                            = false
+  storage_encrypted                   = true
+  publicly_accessible                 = false
+  db_subnet_group_name                = aws_db_subnet_group.db.name
+  skip_final_snapshot                 = true
+  iam_database_authentication_enabled = true
 }
