79163042b7f1b00bd9a689ca32a96a6408a2ba8c
monukoni <kartoplya213@gmail.com>
2026-03-05 20:43:54 +0200

    added alb for frontend

--- terraform/layers/app/main.tf
+++ terraform/layers/app/main.tf
@@ -46,6 +46,68 @@ resource "aws_ecs_task_definition" "frontend" {
   tags = var.tags
 }
 
+resource "aws_security_group" "alb" {
+  name        = "${var.name}-alb-sg"
+  description = "Security group for the Application Load Balancer"
+  vpc_id      = data.terraform_remote_state.bootstrap.outputs.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = var.tags
+}
+
+resource "aws_lb" "frontend" {
+  name               = "${var.name}-frontend-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = data.terraform_remote_state.bootstrap.outputs.public_subnets
+
+  tags = var.tags
+}
+
+resource "aws_lb_target_group" "frontend" {
+  name        = "${var.name}-frontend-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = data.terraform_remote_state.bootstrap.outputs.vpc_id
+  target_type = "ip"
+
+  health_check {
+    path                = "/"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    timeout             = 5
+    interval            = 30
+    matcher             = "200-399"
+  }
+
+  tags = var.tags
+}
+
+resource "aws_lb_listener" "frontend" {
+  load_balancer_arn = aws_lb.frontend.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend.arn
+  }
+}
+
 resource "aws_ecs_service" "frontend" {
   name            = "${var.name}-frontend"
   cluster         = data.terraform_remote_state.ops.outputs.cluster_name
@@ -54,12 +116,18 @@ resource "aws_ecs_service" "frontend" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    # If there is no ALB, the Fargate task may need to be in a public subnet with a public IP 
-    # so you can reach it over the internet.
-    subnets          = data.terraform_remote_state.bootstrap.outputs.public_subnets
+    subnets          = data.terraform_remote_state.bootstrap.outputs.private_subnets
     security_groups  = [data.terraform_remote_state.ops.outputs.ecs_tasks_security_group_id]
-    assign_public_ip = true
+    assign_public_ip = false
   }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.frontend.arn
+    container_name   = "frontend"
+    container_port   = 80
+  }
+
+  depends_on = [aws_lb_listener.frontend]
 
   tags = var.tags
 }
