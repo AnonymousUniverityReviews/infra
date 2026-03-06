dc676305e45fdea1026b47e0ab72c46427621a9e
monukoni <kartoplya213@gmail.com>
2026-03-06 08:54:59 +0200

    added healthcheck for frontend

--- terraform/layers/app/main.tf
+++ terraform/layers/app/main.tf
@@ -21,7 +21,7 @@ resource "aws_ecs_task_definition" "frontend" {
 
   container_definitions = jsonencode([{
     name      = "frontend"
-    image     = "${local.account_id}.dkr.ecr.${var.region}.amazonaws.com/frontend:latest"
+    image     = "${local.account_id}.dkr.ecr.${var.region}.amazonaws.com/frontend:dev-aababdd"
     essential = true
     portMappings = [{
       containerPort = 80
@@ -40,6 +40,13 @@ resource "aws_ecs_task_definition" "frontend" {
         "awslogs-region"        = var.region
         "awslogs-stream-prefix" = "ecs"
       }
+    }
+    healthCheck = {
+      command     = ["CMD-SHELL", "curl -f http://localhost:80/ || exit 1"]
+      interval    = 30
+      timeout     = 5
+      retries     = 3
+      startPeriod = 60
     }
   }])
 }
